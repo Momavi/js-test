04caaa533f7166132e057abda32b8acbf6419dce
Erbis Vladislav <cool.ru1960@gmail.com>
2023-09-22 16:33:22 +0300

    [ADD] Добавил картинки заглушки

--- src/store/apiReguests.js
+++ src/store/apiReguests.js
@@ -6,4 +6,30 @@ const instance = axios.create({
 
 export default {
   async getEmployee({ commit }, data) {
+    data = data.replace(/ /g, "");
+    commit("setLoading", true);
+
+    try {
+      const dataArray = data.split(",");
+      const arrayToOut = await Promise.all(
+        dataArray.map(async (item) => {
+          if (Number.isInteger(Number(item))) {
+            // Если ввели Id
+            const response = await instance.get(`/${item}`);
+            return response.data;
+          } else {
+            // Если ввели имя
+            const response = await instance.get(`?username=${item}`);
+            return response.data[0];
+          }
+        })
+      );
+
+      commit("setEmployee", arrayToOut);
+    } catch (err) {
+      console.error("Error:", err);
+    } finally {
+      commit("setLoading", false);
+    }
+  },
 };
